8698f0c65529dd9cadadf2ada7a2a985a95141af
andre groseth <andregroseth@hotmail.com>
2019-12-06 07:58:30 +0100

    using propTypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
@@ -24,5 +26,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.PropTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
